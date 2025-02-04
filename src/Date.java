9ab1765d9cb3cba3edf2384a803d02c1a7c02f7c
LEPAPE-Lucas-24009890 <lucas.lepape.1@etu.univ-amu.fr>
2025-02-04 16:13:31 +0100

    Création de la class Date + test

---
+++ src/Date.java
@@ -0,0 +1,52 @@
+
+public class Date {
+	private int day;
+	private int month;
+	private int year;
+	
+	public Date() {
+		day = 1;
+		month = 2;
+		year = 2024;
+	}
+	
+	public Date(int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public int getDay() {
+		return day;
+	}
+	
+	public void setDay(int newDay) {
+		this.day = newDay;
+	}
+	
+	public int getMonth() {
+		return month;
+	}
+	
+	public void setMonth(int newMonth) {
+		this.month = newMonth;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int newYear) {
+		this.year = newYear;
+	}
+	
+	public void setDate(int newDay, int newMonth, int newYear) {
+		this.day = newDay;
+		this.month = newMonth;
+		this.year = newYear;
+	}
+	
+	public String toString() {
+		return String.format("Account[Date = %d/%d/%d]", this.day, this.month, this.year);
+	}
+}
